4488c2106459b82e08e3ac3d9183e5e92a2e6453
khurram-pervaiz <67065884+khurram-pervaiz@users.noreply.github.com>
2022-09-22 17:48:46 +0500

    Update LoginTest.java

--- src/test/java/Package_IdentifyU/LoginTest.java
+++ src/test/java/Package_IdentifyU/LoginTest.java
@@ -1,6 +1,5 @@
 package Package_IdentifyU;
 
-import org.aspectj.lang.annotation.After;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterClass;
